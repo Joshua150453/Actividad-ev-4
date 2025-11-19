8890cdbac6107aaae19340cd0e6e47d936c9b574
Joshua150453 <jortizr@ulasalle.edu.pe>
2025-11-19 13:07:09 -0500

    Creando archivo con los datos
    Define a simple User model for CRUD operations.

---
+++ lib/models/user.dart
@@ -0,0 +1,7 @@
+// Modelo simple de usuario que se usa en el CRUD.
+class User {
+  String name;  // Nombre del usuario.
+  int age;      // Edad del usuario.
+
+  User({required this.name, required this.age});
+}
