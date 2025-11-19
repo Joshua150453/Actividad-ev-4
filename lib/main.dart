62193448d56f3608c6a94b94dad3139258ac04b4
Joshua150453 <jortizr@ulasalle.edu.pe>
2025-11-19 13:05:20 -0500

    creando main_dart

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'viewmodels/user_view_model.dart';
+import 'views/login_screen.dart';
+
+void main() {
+  runApp(
+    // Provider que permite usar UserViewModel en toda la app.
+    ChangeNotifierProvider(
+      create: (_) => UserViewModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,   // Oculta la banda de debug.
+      title: 'Login + CRUD',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,        // Tema general.
+      ),
+      home: const LoginScreen(),           // Primera pantalla que se muestra.
+    );
+  }
+}
